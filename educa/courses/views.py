2c2c192513ff7bc8ef8a28c2033e2714702c4acd
SurajBansiKumar <surajbansikumar@gmail.com>
2024-03-24 09:57:41 +0530

    fixed landing page

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    context = {}
+    template = 'courses/index.html'
+    return render(request, template, context)
